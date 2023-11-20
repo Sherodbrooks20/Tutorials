4927c6b3473ed95f931a91d36b5407dfad4a17c2
Sherodbrooks20 <131418644+Sherodbrooks20@users.noreply.github.com>
2023-11-19 19:30:14 -0500

    Add files via upload

---
+++ password_manager_beta_1.py
@@ -0,0 +1,31 @@
+#password_manager_beta_1
+
+master_pwd = input("What is the master password: ")
+
+def view():
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: ", user, "Password: ", passw)
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open("passwords.txt", "a") as f:
+        f.write(name + "|" + pwd + "\n")
+
+
+while True:
+    mode = input("Add a new password or view existing ones, (enter add or view) press q to quit: ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode")
+        continue
